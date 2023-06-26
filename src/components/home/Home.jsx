8c16e517a8cbcfbab5c4aeea98d7ab64ac114f37
AngelMariyaThomas <angelmariyathomas2019@gmail.com>
2023-06-26 17:15:23 +0530

    fourthcommit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.scss'
 import { QuestionState } from '../context/Context'
 import { useQuiz } from '../../hooks/Hooks'
+import { Modal, Button } from 'react-bootstrap'
+
 
 const Home = () => {
   const { QuestionArray } = QuestionState()
-  const {
+  var {
     index,
     score,
+    show,
     color,
     disabled,
     selected,
+    handleClose,
     handleClick,
     next,
     getCurrentQuestion,
     isQuizOver
   } = useQuiz(QuestionArray)
+  const [click,setClick] = useState(true)
+  const clickedft = ()=>{
+    setClick(false)
+  }
+ 
 
   return (
     isQuizOver() ? (
@@ -25,12 +34,12 @@ const Home = () => {
           {score < QuestionArray.length - 1 ? (
             <>
               <h3>Try Again..!</h3>
-              <span>Your score: {score}/5</span>
+              <span>Your score: {score}/15</span>
             </>
           ) : (
             <>
               <h3>Well played!</h3>
-              <span>Your score: {score}/5</span>
+              <span>Your score: {score}/15</span>
             </>
           )}
         </div>
@@ -41,8 +50,9 @@ const Home = () => {
         <div className='card'>
           {[getCurrentQuestion()].map((item) => (
             <div key={item.question}>
-              <h5 style={{ marginTop: "0", color: "white" }}>Question NO : {index + 1}/5</h5>
+          
               {item.question}
+              <br/><br/>
               <ul>
                 {item.options.map((prod, key) => (
                   <div key={key}>
@@ -58,12 +68,39 @@ const Home = () => {
               </ul>
             </div>
           ))}
-          <button className='next' onClick={next}>Next&gt;&gt;</button>
+          <button className='next' onClick={next}>Next&gt;&gt;
+
+          </button>
+
+
         </div>
-        <p>Score: {score}/5</p>
+        <p>Score: {score}/15</p>
+        {console.log(show, score, index)}
+
+        {score== 5 ? (<Modal show={click} onHide={clickedft}
+
+          
+          size="sm"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header >
+            <Button style={{backgroundColor: "yellow" ,color:"black" ,width:"60px", height:"40px"}}onClick={clickedft}>claim</Button>
+            <img  style= {{width:'70px',height:'80px'}}src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmyeAywJ8n5-r54CijFOm4jmUOi40w0Y7TOA&usqp=CAU'/>
+            <Modal.Title id="contained-modal-title-vcenter">
+              5 Points 
+              BONUS!
+            </Modal.Title>
+          </Modal.Header>
+       
+
+        </Modal>) : ("")}
+
       </div>
+
     )
   )
+
 }
 
 export default Home
--- src/hooks/Hooks.jsx
+++ src/hooks/Hooks.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+
+import { useState } from 'react';
 
 export const useQuiz = (QuestionArray) => {
   const [state, setState] = useState({
@@ -6,8 +7,10 @@ export const useQuiz = (QuestionArray) => {
     score: 0,
     color: false,
     disabled: false,
-    selected: null
-  })
+    selected: null,
+    show:false
+
+  });
 
   const handleClick = (prod, answer, key) => {
     if (prod === answer) {
@@ -17,39 +20,63 @@ export const useQuiz = (QuestionArray) => {
         selected: null,
         color: true,
         disabled: true
-      }))
+      }));
     } else {
       setState(prevState => ({
         ...prevState,
         selected: key,
         color: false,
         disabled: true
-      }))
+      }));
     }
-  }
+  };
 
   const next = () => {
-    setState(prevState => ({
+    setState(prevState => {
+      const updatedIndex = prevState.index + 1;
+      let updatedScore = prevState.score;
+      
+      if (updatedIndex ===5) {
+    
+        updatedScore +=5; 
+      }
+      
+      return {
+        ...prevState,
+
+        disabled: false,
+        selected: null,
+        index: updatedIndex,
+        score: updatedScore
+      };
+    });
+  };
+  
+  const handleClose=()=>{
+    setState((prevState)=>({
       ...prevState,
-      disabled: false,
-      selected: null,
-      index: prevState.index + 1
+      show:true,
+      score:prevState.score+5,
+    
     }))
   }
 
-  const { index, score, color, disabled, selected } = state
+  const { index, score, color, disabled, selected } = state;
 
-  const getCurrentQuestion = () => QuestionArray[index]
-  const isQuizOver = () => index > QuestionArray.length - 1
+  const getCurrentQuestion = () => QuestionArray[index];
+  const isQuizOver = () => index > QuestionArray.length - 1;
 
   return {
     score,
     color,
     disabled,
     selected,
+    handleClose,
     handleClick,
     next,
     getCurrentQuestion,
     isQuizOver
-  }
-}
+  };
+};
+
+
